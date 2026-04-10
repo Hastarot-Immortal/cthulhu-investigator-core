fef92a3ca271059dd253d6a25e9f209f288a5ff4
Hastarot-Immortal <hastarotimmortal@gmail.com>
2026-04-10 16:22:28 +0300

    Add support ==, >, <, >=, <= with u8 for Skill

--- src/attributes/skill.rs
+++ src/attributes/skill.rs
@@ -1,4 +1,5 @@
 use std::ops;
+use std::cmp::{PartialEq, PartialOrd, Ordering};
 
 fn validate_skill(value: u8) -> u8 {
 	value.min(99) 
@@ -42,5 +43,29 @@ impl ops::Sub<u8> for Skill {
 impl ops::SubAssign<u8> for Skill {
 	fn sub_assign(&mut self, rhs: u8) {
 		self.0 = sub_skill(self.0, rhs); 
+	}
+}
+
+impl PartialEq<u8> for Skill {
+	fn eq(&self, other: &u8) -> bool {
+		self.0 == *other
+	}
+}
+
+impl PartialOrd<u8> for Skill {
+	fn partial_cmp(&self, other: &u8) -> Option<Ordering> {
+		Some(self.0.cmp(other))
+	}
+}
+
+impl PartialEq<Skill> for u8 {
+	fn eq(&self, other: &Skill) -> bool {
+		*self == other.0
+	}
+}
+
+impl PartialOrd<Skill> for u8 {
+	fn partial_cmp(&self, other: &Skill) -> Option<Ordering> {
+		Some(self.cmp(&other.0))
 	}
 }
